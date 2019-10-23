4d667864750a579f7a6ecc1f959fb8539173784b
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-10-23 14:59:23 -0400

    update credit

--- credit.c
+++ credit.c
@@ -1,15 +1,28 @@
 #import <cs50.h>
 #import <stdio.h>
 
+// Test numbers:
+// 378282246310005 (Amex)
+// 371449635398431 (Amex)
+// 5555555555554444 (MasterCard)
+// 5105105105105100 (MasterCard)
+// 4111111111111111 (Visa)
+// 4012888888881881 (Visa)
+// 4222222222222 (Visa)
+
+// Failing numbers:
+// 4222222222222 (13)
+// 5105105105105100 (Prints INVALID and MASTERCARD)
+
 void digits(int *buf, long int cc)
 {
-    int count = 0;
+    int i = 0;
     long int prev_cc = cc;
     do {
         cc = prev_cc / 10;
-        buf[count] = prev_cc % 10;
+        buf[i] = prev_cc % 10;
         prev_cc = cc; 
-        count++;
+        i++;
     } while ( cc > 0 );
 }
 
@@ -36,7 +49,7 @@ int sum_rest(int *buf)
     return count;
 }
 
-bool check_valid(int sum)
+bool is_valid(int sum)
 {
     if (sum % 2 == 0) return true;
     else return false;
@@ -59,15 +72,25 @@ int main(void)
         cc = get_long("Number: "); 
     } while (cc == 0);
     
+    if (cc < 1000000000000)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+    
     // buffer[17] for max cc number of 16, plus 1
     int buffer[17];
     // fill buffer with cc digits
     digits(buffer, cc);
-
-    // for(int i = 0; i < 16; i++) printf("%i ", buffer[i]);
-    int sum = multiply_and_sum_digits(buffer) + sum_rest(buffer);
     
-    if (!check_valid(sum)) printf("INVALID\n");
+    int sum = multiply_and_sum_digits(buffer) + sum_rest(buffer);
+    printf("SUM: %i\n", sum);
+    
+    if (!is_valid(sum)) 
+    {
+        printf("INVALID\n");
+        return 0;
+    }
     
     if (first_digit(cc) == 3) printf("AMEX\n");   
     if (first_digit(cc) == 4) printf("VISA\n");
